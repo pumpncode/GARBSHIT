4405cbd37e91e5e507db18acdbd9f8c2dfc6e2b0
Gainumki <gabrytechgamer@gmail.com>
2025-03-06 18:14:00 +0100

    Aeon!

--- main.lua
+++ main.lua
@@ -49,6 +49,13 @@ SMODS.Atlas{
     py = 95 -- height of one card
 }
 
+SMODS.Atlas{
+  key = 'GarbConsumables', --atlas key
+  path = 'Consumables.png', --atlas' path in (yourMod)/assets/1x or (yourMod)/assets/2x
+  px = 65, --width of one card
+  py = 95 -- height of one card
+}
+
 	SMODS.Sound {
     key = "mirrorz",
     path = {
@@ -289,6 +296,37 @@ function Card:use_consumeable(area, copier)
   return ret
 end
 
+-- CONSUMABLES
+SMODS.Consumable{
+  key = 'aeon',
+  set = 'Spectral',
+  loc_txt = {
+    name = 'The Aeon',
+    text = {
+	  "Creates a {E:1,C:dark_edition}Negative{} copy",
+    "of selected {C:attention}Joker{}"
+    }
+  },
+  atlas = 'GarbConsumables', pos = { x = 0, y = 0 },
+  hidden = {soul_set = 'Tarot'},
+  soul_pos = { x = 1, y = 0 },
+
+  can_use = function(self, card)
+    if #G.jokers.highlighted == 1 then return true else return false end
+  end,
+
+  use = function(self, card, area, copier)
+    G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+      play_sound('timpani')
+      local new_card = SMODS.create_card{key = G.jokers.highlighted[1].config.center_key, edition = "e_negative"}
+      new_card:add_to_deck()
+      G.jokers:emplace(new_card)
+      card:juice_up(0.3, 0.5)
+      return true end }))
+    delay(0.6)
+    return true
+  end
+}
 -- JOKERS
 
 SMODS.Joker {
