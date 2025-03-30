b909edf5be7854adbbac6b6bf717bf80a2f558ce
Gainumki <gabrytechgamer@gmail.com>
2025-03-30 18:27:40 +0200

    patched onibi but fr

--- scripts/jokers.lua
+++ scripts/jokers.lua
@@ -992,7 +992,7 @@ SMODS.Joker {
     
       unlocked = true, 
       discovered = false, --whether or not it starts discovered
-      blueprint_compat = false, --can it be blueprinted/brainstormed/other
+      blueprint_compat = true, --can it be blueprinted/brainstormed/other
       eternal_compat = true, --can it be eternal
       perishable_compat = true, --can it be perishable
       cost = 5,
@@ -1001,12 +1001,10 @@ SMODS.Joker {
     end,
       
      calculate = function(self, card, context)
-      if context.selling_card and not context.blueprint and context.card.config.center.set == "Joker" and pseudorandom('ONIBI') < G.GAME.probabilities.normal/card.ability.extra.odds then
+      if context.selling_card and context.card.config.center.set == "Joker" and pseudorandom('ONIBI') < G.GAME.probabilities.normal/card.ability.extra.odds then
           if #G.consumeables.cards + G.GAME.consumeable_buffer < G.consumeables.config.card_limit then
                       G.GAME.consumeable_buffer = G.GAME.consumeable_buffer + 1
                       G.E_MANAGER:add_event(Event({
-                          trigger = 'before',
-                          delay = 0.0,
                           func = (function()
                                   local card = create_card('Spectral',G.consumeables, nil, nil, nil, nil, nil, 'sixth')
                                   card:add_to_deck()
@@ -1016,11 +1014,9 @@ SMODS.Joker {
                           end)}))
                       card_eval_status_text(context.blueprint_card or card, 'extra', nil, nil, nil, {message = localize('k_plus_spectral'), colour = G.C.SECONDARY_SET.Spectral})
                   end
-          return true
       end
     end
-  },
-  
+  },  
   SMODS.Joker {
     key = 'equality',
     loc_txt = {
