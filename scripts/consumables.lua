816adf41763ed93c8b28eca0a52bc7098ac8e5fe
Gainumki <gabrytechgamer@gmail.com>
2025-04-07 21:31:58 +0200

    matcha

--- scripts/consumables.lua
+++ scripts/consumables.lua
@@ -375,8 +375,8 @@ SMODS.Consumable{
     name = 'Breakfast',
     text = {
       "Exchange selected Joker",
-      "for {C:attention}+#1#{} hands",
-      "next round"
+      "for {C:chips}+#1#{} hands",
+      "for one round"
     }
   },
     atlas = 'Stamps', pos = { x = 3, y = 1 },
@@ -400,7 +400,6 @@ SMODS.Consumable{
       ease_hands_played(card.ability.extra.hand)
       delay(0.6)
     end
-
 },
 
 SMODS.Consumable{
@@ -445,6 +444,40 @@ SMODS.Consumable{
       delay(0.6)
     end
 
+},
+
+SMODS.Consumable{
+  key = 'matcha',
+  set = 'Stamp',
+  loc_txt = {
+    name = 'Matcha',
+    text = {
+      "Exchange selected Joker",
+      "for {C:mult}+#1#{} discards",
+      "for one round"
+    }
+  },
+    atlas = 'Stamps', pos = { x = 0, y = 2 },
+
+    config = {extra = { discards = 3 }},
+    
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.discards }}
+    end,
+
+    can_use = function(self, card)
+      if #G.jokers.highlighted == 1 and not G.jokers.highlighted[1].ability.eternal then return true else return false end
+    end,
+  
+    use = function(self, card, area, copier)
+      play_sound('timpani')
+      _card = G.jokers.highlighted[1]
+      _card:start_dissolve(nil, false)
+      G.GAME.round_resets.temp_discards = (G.GAME.round_resets.temp_discards or 0) + card.ability.extra.discards
+      G.GAME.round_resets.discards = G.GAME.round_resets.discards + card.ability.extra.discards
+      ease_discard(card.ability.extra.discards)
+      delay(0.6)
+    end
 },
 
 -- MISC CONSUMABLES
