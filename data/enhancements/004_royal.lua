6f227ef8043fd51fb998c2dfb542e212b0b217d0
Gainumki <gabrytechgamer@gmail.com>
2025-05-26 21:59:13 +0200

    Royal Cards hotfix

--- data/enhancements/004_royal.lua
+++ data/enhancements/004_royal.lua
@@ -3,7 +3,6 @@ return {
 	key = "royal",
 	atlas = "GarbEnhancements",
 	pos = {x = 3, y = 0},
-	
     replace_base_card = false,
     no_suit = false,
     no_rank = false,
@@ -11,30 +10,32 @@ return {
 	
 	config = {extra = {active = false }},
 	
-	loc_vars = function(self, info_queue, card)
-        return { vars = {  }}
-    end,
-  
-  calculate = function(self, card, context)
-    if context.hand_drawn and context.cardarea == G.hand and not card.ability.extra.active then
+  update = function(self, card)
+    if not card.R_active and not card.debuff and card.area and card.area == G.hand then 
       G.hand:change_size(1)
-      card.ability.extra.active = true
+      card.R_active = true
     end
 
-    if context.after then
-      if card.ability.extra.active then
-        card.ability.extra.active = false
-        G.hand:change_size(-1)
-      end
-      if context.cardarea == G.hand then
-        draw_card(G.hand,G.discard, 90, 'up', nil, card)  
-      end
+    if card.R_active and card.area and card.area ~= G.hand then 
+      G.hand:change_size(-1)
+      card.R_active = false
     end
   end,
 
+	loc_vars = function(self, info_queue, card)
+        return { vars = {  }}
+  end,  
+
   remove_from_deck = function(self, card, from_debuff)
-    if card.ability.extra.active then
+    if card.R_active then
+      card.R_active = false
       G.hand:change_size(-1)
+    end
+  end,
+
+  calculate = function(self, card, context)
+    if card.R_active and context.after then
+      draw_card(G.hand,G.discard, 90, 'up', nil, card)
     end
   end
 }
